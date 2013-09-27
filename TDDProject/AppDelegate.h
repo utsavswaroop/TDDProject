4e5f3af9928480eb4bd0a5086f63974c42de5982
Utsav Patel <utsav.swaroop@gmail.com>
2013-09-27 13:56:37 +0100

    Initial Commit

---
+++ TDDProject/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TDDProject
+//
+//  Created by Utsav on 27/09/2013.
+//  Copyright (c) 2013 Utsav. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TDDProject/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TDDProject
+//
+//  Created by Utsav on 27/09/2013.
+//  Copyright (c) 2013 Utsav. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
